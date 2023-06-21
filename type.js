278c5a2dc8bd4ef8f0b8989deba41e6d9ef74769
ealande <erickalan23@gmail.com>
2023-06-20 21:07:04 -0300

    compiled ts

---
+++ type.js
@@ -0,0 +1,8 @@
+function multiply(fator1, fator2) {
+    return fator1 * fator2;
+}
+console.log(multiply(2, 3));
+function hello(name) {
+    return 'Olá, ' + name;
+}
+console.log(hello("erick"));
