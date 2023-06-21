786138cdb914cb1b68d47cc4a2afb640d4a85559
ealande <erickalan23@gmail.com>
2023-06-20 22:23:46 -0300

    type inserted to the function return

--- type.ts
+++ type.ts
@@ -1,10 +1,10 @@
-function multiply(fator1:number, fator2:number){
+function multiply(fator1:number, fator2:number): number{
     return fator1 * fator2;
 }
 
 console.log(multiply(2, 3));
 
-function hello(name:string){
+function hello(name:string): string{
 	return 'Olá, ' + name;
 }
 console.log(hello("erick"));
