9451bed75bc3a0f1d550aaa8ac050f8b58fd98da
ealande <erickalan23@gmail.com>
2023-06-20 21:03:12 -0300

    ts compiled

---
+++ type.ts
@@ -0,0 +1,10 @@
+function multiply(fator1:number, fator2:number){
+    return fator1 * fator2;
+}
+
+console.log(multiply(2, 3));
+
+function hello(name:string){
+	return 'Olá, ' + name;
+}
+console.log(hello("erick"));
